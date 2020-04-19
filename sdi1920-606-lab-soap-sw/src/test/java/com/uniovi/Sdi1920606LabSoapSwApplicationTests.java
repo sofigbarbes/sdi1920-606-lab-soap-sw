f4eb292260e37ff8f074c44a033bad9278415235
UO263909 <UO263909@uniovi.es>
2020-04-19 17:38:27 +0200

    SDI-SW-11.1 Servicio SOAP

---
+++ sdi1920-606-lab-soap-sw/src/test/java/com/uniovi/Sdi1920606LabSoapSwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi1920606LabSoapSwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
